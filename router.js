4f2c10d0e7853fe7d2067f29d04fdca3ba420a97
Oleg Esikov <esikov.oleg@mail.ru>
2021-11-01 01:25:17 +0300

    Реализовано добавление пользовательского ввода в шаблон перед рендерингом

--- router.js
+++ router.js
@@ -1,12 +1,30 @@
 const express = require('express');
 const pug = require('pug');
+const fs = require('fs');
 
 const router = express.Router();
 
 router.get("/", (req, res) => {
-    const input = "= 7 * 7";
-    const test = pug.compile(`${input}`);
-    res.render("test", {name: input, testName: test(), abc: `${input}`});
+    res.render("index");
+});
+
+router.post("/comments/clear/", (req, res) => {
+    const {
+        code
+    } = req.body;
+
+    let template = fs.readFileSync('./views/index.pug', 'utf8');
+    if (typeof code != 'undefined'){
+        template = template.replace(/=resultCode/g, code);
+    }
+
+    let html = pug.render(template);
+    res.set('Content-Type', 'text/html');
+    res.send(html);
+
+    //const input = "#{root.process.mainModule.require('child_process').execSync('ls')}";
+    //const output = pug.compile(`p ${code}`)();
+    //res.render("index", {resultCode: output});
 });
 
 module.exports = router;
