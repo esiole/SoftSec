7538fce749b281c49fbcabda6a11d38da64cdac5
Oleg Esikov <esikov.oleg@mail.ru>
2021-11-11 22:34:18 +0300

    Перенёс SSTI на форму отправления отзыва

--- router.js
+++ router.js
@@ -18,18 +18,31 @@ router.get("/help", (req, res) => {
     res.render("help");
 });
 
-router.post("/comments/clear/", (req, res) => {
+router.post("/comments/clear", (req, res) => {
     const {
         code,
         lang
     } = req.body;
 
     const resultCode = utils.clearComments(typeof code != 'undefined' ? code : "", lang)
-    let template = fs.readFileSync('./views/index.pug', 'utf8');
-    template = template.replace(/=code/g, code);
-    template = template.replace(/=resultCode/g, resultCode);
+    res.render("index", {code: code, resultCode: resultCode, lang: lang});
+});
 
-    let html = pug.render(template, {lang: lang});
+router.get("/report", (req, res) => {
+    res.render("report");
+});
+
+router.post("/report", (req, res) => {
+    const {
+        name,
+        email,
+        problem
+    } = req.body;
+
+    let template = fs.readFileSync('./views/thanks.pug', 'utf8');
+    template = template.replace(/#{name}/g, name);
+
+    let html = pug.render(template, {name: name});
     res.set('Content-Type', 'text/html');
     res.send(html);
 });
