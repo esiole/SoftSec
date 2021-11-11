7b5a92243848753f19b340c982ad281b2315c60b
Oleg Esikov <esikov.oleg@mail.ru>
2021-11-11 22:46:25 +0300

    Добавил мастер-страницу

--- router.js
+++ router.js
@@ -6,12 +6,7 @@ const utils = require('./utils.js');
 const router = express.Router();
 
 router.get("/", (req, res) => {
-    let template = fs.readFileSync('./views/index.pug', 'utf8');
-    template = template.replace(/=code/g, "");
-    template = template.replace(/=resultCode/g, "");
-    let html = pug.render(template);
-    res.set('Content-Type', 'text/html');
-    res.send(html);
+    res.render("index");
 });
 
 router.get("/help", (req, res) => {
